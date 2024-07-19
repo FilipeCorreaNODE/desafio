ea72bbec3664ff8efb655d82de88042317f40b2f
Filipe Correa <filipe.rocha.correa@gmail.com>
2024-07-19 17:07:08 -0300

    Ajuste de tretamento de retorno dos metodos da controler

--- agenda/src/main/java/com/noxtec/desafio/agenda/controller/ContatoController.java
+++ agenda/src/main/java/com/noxtec/desafio/agenda/controller/ContatoController.java
@@ -1,12 +1,11 @@
 package com.noxtec.desafio.agenda.controller;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,100 +20,68 @@ import com.noxtec.desafio.agenda.service.ContatoService;
 
 @RestController
 @RequestMapping("/desafio/agenda")
+@CrossOrigin("*")
 public class ContatoController {
 
 	@Autowired
 	private ContatoService contatoService;
 
 	@PostMapping("/save")
-	public ResponseEntity<?> save(@RequestBody Contato contato) {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
+	public ResponseEntity<String> save(@RequestBody Contato contato) {
 		try {
 			String mensagem = contatoService.save(contato);
-			map.put("status", 1);
-			map.put("message", mensagem);
-			return new ResponseEntity<>(map, HttpStatus.CREATED);
-
+			return new ResponseEntity<>(mensagem, HttpStatus.CREATED);
 		} catch (Exception e) {
-			map.put("status", 0);
-			map.put("message", e.getMessage());
-			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 	}
 
 	@PutMapping("/update/{id}")
-	public ResponseEntity<?> update(@RequestBody Contato contato, @PathVariable long id) {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
+	public ResponseEntity<String> update(@RequestBody Contato contato, @PathVariable long id) {
 		try {
 			String mensagem = contatoService.update(contato, id);
-			map.put("status", 1);
-			map.put("message", mensagem);
-			return new ResponseEntity<>(map, HttpStatus.OK);
+			return new ResponseEntity<>(mensagem, HttpStatus.OK);
 
 		} catch (Exception e) {
-			map.put("status", 0);
-			map.put("message", e.getMessage());
-			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 	}
 
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<?> delete(@PathVariable long id) {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
+	public ResponseEntity<String> delete(@PathVariable long id) {
 		try {
 			String mensagem = contatoService.delete(id);
 			if (mensagem != null) {
-				map.put("status", 1);
-				map.put("message", mensagem);
-				return new ResponseEntity<>(map, HttpStatus.OK);
+				return new ResponseEntity<>(mensagem, HttpStatus.OK);
 			} else {
-				map.clear();
-				map.put("status", 0);
-				map.put("message", "Não encontrou o contato.");
-				return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>("Não encontrou o contato.", HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			map.put("status", 0);
-			map.put("message", e.getMessage());
-			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 	}
 
 	@GetMapping("/findAll")
-	public ResponseEntity<?> findAll() {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
+	public ResponseEntity<List<Contato>> findAll() {
 		try {
 			List<Contato> contatos = this.contatoService.findAll();
 			if (!contatos.isEmpty()) {
-				map.put("status", 1);
-				map.put("data", contatos);
-				return new ResponseEntity<>(map, HttpStatus.OK);
+				return new ResponseEntity<>(contatos, HttpStatus.OK);
 			} else {
-				map.clear();
-				map.put("status", 0);
-				map.put("message", "Não encontrou contatos.");
-				return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			map.put("status", 0);
-			map.put("message", e.getMessage());
-			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 	}
 
 	@GetMapping("/findById/{id}")
-	public ResponseEntity<?> findById(@PathVariable long id) {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
+	public ResponseEntity<Contato> findById(@PathVariable long id) {
 		try {
 			Contato contato = this.contatoService.findById(id);
-			map.put("status", 1);
-			map.put("data", contato);
-			return new ResponseEntity<>(map, HttpStatus.OK);
+			return new ResponseEntity<>(contato, HttpStatus.OK);
 		} catch (Exception e) {
-			map.clear();
-			map.put("status", 0);
-			map.put("message", "Não encontrou o contato.");
-			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 	}
 }
